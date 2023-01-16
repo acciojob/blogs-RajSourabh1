0e7acf832112892bf7534aca50b5a0ad1672720c
Sourabh <createownpath111@gmail.com>
2023-01-16 22:59:44 +0530

    code9

--- src/main/java/com/driver/services/ImageService.java
+++ src/main/java/com/driver/services/ImageService.java
@@ -41,17 +41,31 @@ public class ImageService {
     public int countImagesInScreen(Image image, String screenDimensions) {
         //Find the number of images of given dimensions that can fit in a screen having `screenDimensions`
         //In case the image is null, return 0
-        //int count = 0;
+        int count = 0;
         String str = image.getDimensions();
-            int screenLength = Integer.parseInt(screenDimensions.split("X")[0]);
-            int screenBreadth = Integer.parseInt(screenDimensions.split("X")[1]);
-            int imageLength = Integer.parseInt(str.split("X")[0]);
-            int imageBreadth = Integer.parseInt(str.split("X")[1]);
-
-            int maxLength = screenLength/imageLength;
-            int maxBreadth = screenBreadth/imageBreadth;
-            int count = maxLength*maxBreadth;
+            int lenScreen = 0;
+            int breScreen = 0;
+            for(int i = 0;i<screenDimensions.length();i++){
+                if(screenDimensions.charAt(i)=='X'){
+                    lenScreen = breScreen;
+                    breScreen = 0;
+                    continue;
+                }
+                breScreen *=10;
+                breScreen +=(screenDimensions.charAt(i)-'0');
+            }
+            int lenImage = 0;
+            int breImage = 0;
+        for(int i = 0;i<screenDimensions.length();i++){
+            if(screenDimensions.charAt(i)=='X'){
+                lenImage = breImage;
+                breImage = 0;
+                continue;
+            }
+            breImage *=10;
+            breImage +=(screenDimensions.charAt(i)-'0');
+        }
+         count = (lenScreen/lenImage) * (breScreen/breImage);
             return count;
-
     }
 }
