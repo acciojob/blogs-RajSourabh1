bec02f6483d706b6c14e86d9b73961e956a183ef
Sourabh <createownpath111@gmail.com>
2023-01-17 23:48:22 +0530

    code15

--- src/main/java/com/driver/services/BlogService.java
+++ src/main/java/com/driver/services/BlogService.java
@@ -68,6 +68,9 @@ public class BlogService {
 
     public void deleteBlog(int blogId){
         //delete blog and corresponding images
-        blogRepository1.deleteById(blogId);
+        Blog blog = blogRepository1.findById(blogId).get();
+        if(blog != null){
+            blogRepository1.delete(blog);
+        }
     }
 }
